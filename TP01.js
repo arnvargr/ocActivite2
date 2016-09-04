2992b208cc26edfa0c907034738bb8972ab11957
shaman <arnvargr@hotmail.com>
2016-09-04 09:29:49 +0200

    modificiation du script TP01.js

--- TP01.js
+++ TP01.js
@@ -1,29 +1,66 @@
-/* 
-OpenClassroom :  Dynamisez vos sites web avec JavaScript ! 
-TP 01 : Convertir un nombre en toutes lettres
-*/
+
+function num2Letters(number) {
+
+	if (isNaN(number) || number < 0 || 999 < number) {
+		return 'Veuillez entrer un nombre entier compris entre 0 et 999.';
+	}
+
+	var units2Letters = ['', 'un', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf', 'dix', 'onze', 'douze', 'treize', 'quatorze', 'quinze', 'seize', 'dix-sept', 'dix-huit', 'dix-neuf'],
+		tens2Letters  = ['', 'dix', 'vingt', 'trente', 'quarante', 'cinquante', 'soixante', 'soixante', 'quatre-vingt', 'quatre-vingt'];
+
+	var units    = number % 10,
+		tens     = (number % 100 - units) / 10,
+		hundreds = (number % 1000 - number % 100) / 100;
+
+	var unitsOut, tensOut, hundredsOut;
 
 
-console.log("Bienvenue dans ce TP numéro 1 !");
-var nbrUtilisateur = prompt("Entrez un nombre en 0 et 999");
+	if (number === 0) {
 
-// On teste si l'utilisateur a bien entré un nombre et si le nombre est bien dans la fourchette 0 - 999
+		return 'zéro';
 
-    var testNumeric = parseInt(nbrUtilisateur); // Contient au final la valeur « NaN »
-    if (isNaN(testNumeric) || testNumeric <0 || testNumeric> 999 ){
-         console.log("Alerte !!! Votre entrée n'est pas un nombre ou n'est pas dans la fourchette 0 - 999 ");
-        var nbrUtilisateur = prompt("Entrez un nombre en 0 et 999");
-       
-    }
-else{
-    console.log("Votre entrée est un nombre correct ");
+	} else {
+
+		
+
+		unitsOut = (units === 1 && tens > 0 && tens !== 8 ? 'et-' : '') + units2Letters[units];
+
+
+
+		if (tens === 1 && units > 0) {
+
+			tensOut = units2Letters[10 + units];
+			unitsOut = '';
+
+		} else if (tens === 7 || tens === 9) {
+
+			tensOut = tens2Letters[tens] +'-'+ (tens === 7 && units === 1 ? 'et-' : '') + units2Letters[10 + units];
+			unitsOut = '';
+
+		} else {
+
+			tensOut = tens2Letters[tens];
+
+		}
+
+		tensOut += (units === 0 && tens === 8 ? 's' : '');
+
+	
+		hundredsOut = (hundreds > 1 ? units2Letters[hundreds] + '-' : '') + (hundreds > 0 ? 'cent' : '') + (hundreds > 1 && tens == 0 && units == 0 ? 's' : '');
+
+	
+
+		return hundredsOut + (hundredsOut && tensOut ? '-': '') + tensOut + (hundredsOut && unitsOut || tensOut && unitsOut ? '-': '') + unitsOut;
+	}
+
 }
 
-        
 
-var tailleNbr =  nbrUtilisateur.length;
-console.log("La taille de votre nombre est : " + tailleNbr);
 
-// création d'un tableau à trois entrées
+var userEntry;
 
-console.log("Votre nombre est : " + nbrUtilisateur);
+while (userEntry = prompt('Indiquez le nombre à écrire en toutes lettres (entre 0 et 999) :')) {
+
+	alert(num2Letters(parseInt(userEntry, 10)));
+
+}
